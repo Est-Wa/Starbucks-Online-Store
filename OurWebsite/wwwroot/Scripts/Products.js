8cdf86478b43967cb1f2fa0df7559358a85f94a4
tzwand <tzipwand@gmail.com>
2023-05-11 21:15:39 +0300

    Shopping Bag

--- OurWebsite/wwwroot/Scripts/Products.js
+++ OurWebsite/wwwroot/Scripts/Products.js
@@ -23,6 +23,14 @@ async function getProducts() {
     }
 }
 
+function drawProducts(products) {
+    if (products != null) {
+        const productList = document.getElementById('ProductList')
+        productList.innerHTML = '';
+        products.forEach(product => drawProduct(product))
+    }
+}
+
 function drawProduct(product) {
     let template = document.getElementById("temp-card");
     let clone = template.content.cloneNode(true);
@@ -35,6 +43,8 @@ function drawProduct(product) {
     price.textContent = `${product.price}$`
     let description = clone.querySelector(".description");
     description.textContent = product.description;
+    const addButton = clone.querySelector("button")
+    addButton.addEventListener('click', () => addToCart(product))
     const productList = document.getElementById('ProductList')
     productList.appendChild(clone);
 }
@@ -71,14 +81,44 @@ function drawCategory(category) {
 }
 
 async function onload() {
-    const products = await getProducts();
-    if (products != null) { 
-        const productList = document.getElementById('ProductList')
-        productList.innerHTML = '';
-        products.forEach(product => drawProduct(product))}
-    const categories = await getCategories();
+    const products = [{
+        ProductId: 1, productName: 'Iced Chai Tea Latte', price: 15,
+        Despriction: 'Black tea infused with cinnamon, clove, and other warming spices are combined with milk and ice for the perfect balance of sweet and spicy.',
+        CatergoryId: 1, imageLink: 'IcedChai.jpg'
+    }, {
+        ProductId: 2, productName: 'Green Soft Touch Stainless-Steel Cold Cup', price: 10,
+        Despriction: 'Our 24 fl oz stainless-steel cold cup in a classic green with a soft touch finish adds a special feel.',
+        CatergoryId: 1, imageLink: 'GreenCup.jpg'
+    }, {
+        ProductId: 3, productName: 'Green Soft Touch Stainless-Steel Cold Cup', price: 10,
+        Despriction: 'Our 24 fl oz stainless-steel cold cup in a classic green with a soft touch finish adds a special feel.',
+        CatergoryId: 1, imageLink: 'DoubleChoc.jpg'
+    }, {
+        ProductId: 4, productName: 'Green Soft Touch Stainless-Steel Cold Cup', price: 10,
+        Despriction: 'Our 24 fl oz stainless-steel cold cup in a classic green with a soft touch finish adds a special feel.',
+        CatergoryId: 1, imageLink: 'Espresso.jpg'
+    }, {
+        ProductId: 5, productName: 'Green Soft Touch Stainless-Steel Cold Cup', price: 10,
+        Despriction: 'Our 24 fl oz stainless-steel cold cup in a classic green with a soft touch finish adds a special feel.',
+        CatergoryId: 1, imageLink: 'StrawberryCreme.jpg'
+    }];
+    // await getProducts();
+    drawProducts(products)
+    const categories = null;
+//        await getCategories();
     if (categories != null)
         categories.forEach(category => drawCategory(category))
+    setAmountOfItems()
+}
+
+function setAmountOfItems() {
+    const cart = JSON.parse(localStorage.getItem('cart'))
+    let sum = 0;
+    if (cart) {
+        cart.forEach(p => sum += p.amount)
+    }
+    const itemsCount = document.getElementById('ItemsCountText')
+    itemsCount.textContent = sum
 }
 
 
@@ -98,31 +138,35 @@ async function categoryFilter(categoryId,e) {
     }
     console.log(categoryIds)
     const products = await getProducts();
-    if (products != null) {
-        const productList = document.getElementById('ProductList')
-        productList.innerHTML = '';
-        products.forEach(product => drawProduct(product))
-    }
+    drawProducts(products)
 }
 
 async function nameFilter() {
     const input = document.getElementById('nameSearch')
     productName = input.value
     const products = await getProducts();
-    if (products != null) {
-        const productList = document.getElementById('ProductList')
-        productList.innerHTML = '';
-        products.forEach(product => drawProduct(product))
-    }
+    drawProducts(products)
 }
 
 async function priceFilter() {
     minPrice = document.getElementById('minPrice').value
     maxPrice = document.getElementById('maxPrice').value
     const products = await getProducts();
-    if (products != null) {
-        const productList = document.getElementById('ProductList')
-        productList.innerHTML = '';
-        products.forEach(product => drawProduct(product))
+    drawProducts(products)
+}
+
+function addToCart(product) {
+    let cart = localStorage.getItem('cart')
+    let inCart = false;
+    if (cart != null) {
+        cart = JSON.parse(cart)
+        cart.forEach((p) => { if (p.ProductId == product.ProductId) { p.amount += 1; inCart = true } })
+        if (!inCart) {
+            cart.push({ ...product, amount: 1 }) }
+        localStorage.setItem('cart', JSON.stringify(cart))
     }
+    else {
+        localStorage.setItem('cart', JSON.stringify([{ ...product,amount: 1 }]))
+    }
+    setAmountOfItems()
 }
---
+++ OurWebsite/wwwroot/Scripts/ShoppingBag.js
@@ -0,0 +1,70 @@
+﻿document.addEventListener('load', onload())
+
+
+function onload() {
+    const cart = JSON.parse(localStorage.getItem('cart'))
+    drawProducts(cart)
+}
+
+
+
+function changeAmount(productId, itemNumber, operation) {
+    let products = JSON.parse(localStorage.getItem('cart'))
+    let amount;
+    products.forEach(p => { if (p.ProductId == productId) { operation == '-' ? p.amount -= 1 : p.amount+=1; amount = p.amount } })
+    if (amount == 0) {
+        deleteItem(productId)
+        return
+    }
+    itemNumber.textContent = amount
+   
+    localStorage.setItem('cart', JSON.stringify(products))
+}
+
+function deleteItem(productId) {
+    let products = JSON.parse(localStorage.getItem('cart'))
+    products = products.filter(p => p.ProductId != productId)
+    localStorage.setItem('cart', JSON.stringify(products))
+    drawProducts(products)
+}
+
+function drawProducts(products) {
+    let items = document.querySelector('#items tbody')
+    items.innerHTML = ''
+    if (products.length > 0)
+        products.forEach((p) => drawProduct(p))
+}
+
+function drawProduct(product) {
+    let template = document.getElementById("temp-row");
+    console.log(template)
+    let clone = template.content.cloneNode(true);
+
+    let img = clone.querySelector('.imageColumn .image')
+    img.src = `../Images/Products/${product.imageLink}`;
+    img.alt = product.imageLink;
+
+    let itemName = clone.querySelector('.descriptionColumn .itemName')
+    itemName.textContent = product.productName
+
+    let itemNumber = clone.querySelector('.amountColumn h3')
+    itemNumber.textContent = product.amount
+
+    let more = clone.querySelector('.amountColumn #more')
+    more.addEventListener('click', () => { changeAmount(product.ProductId, itemNumber,'+') })
+
+    let less = clone.querySelector('.amountColumn #less')
+    less.addEventListener('click', () => { changeAmount(product.ProductId, itemNumber,'-') })
+
+    let deleteButton = clone.querySelector('#deleteButton')
+    console.log(deleteButton)
+    deleteButton.addEventListener('click', () => { deleteItem(product.ProductId) })
+
+
+    let items = document.querySelector('#items tbody')
+    items.appendChild(clone);
+}
+
+
+
+
